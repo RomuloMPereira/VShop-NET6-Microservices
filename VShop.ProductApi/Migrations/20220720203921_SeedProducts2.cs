a70e56b30267be3fc94eb2c7bebad81bd04add6e
ROMULO\mprom <mp.romulo@gmail.com>
2022-07-20 17:44:24 -0300

    Add Migrations (ProductAPI)

---
+++ VShop.ProductApi/Migrations/20220720203921_SeedProducts2.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VShop.ProductApi.Migrations
+{
+    public partial class SeedProducts2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("Insert into Products(Name, Price, Description, Stock, ImageUrl, CategoryId)" +
+                " Values('Caderno', 7.55, 'Caderno', 10, 'caderno1.jpg', 1)");
+
+            migrationBuilder.Sql("Insert into Products(Name, Price, Description, Stock, ImageUrl, CategoryId)" +
+                " Values('Lápis', 3.45, 'Lápis Preto', 20, 'lapis1.jpg', 1)");
+
+            migrationBuilder.Sql("Insert into Products(Name, Price, Description, Stock, ImageUrl, CategoryId)" +
+                " Values('Clips', 5.30, 'Clips para papel', 50, 'clips1.jpg', 2)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("Delete from Products");
+        }
+    }
+}
