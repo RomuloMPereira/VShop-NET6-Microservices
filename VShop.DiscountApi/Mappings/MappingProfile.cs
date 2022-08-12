9815639777239e86b35783f4cb5d9a1d6cb3e29b
ROMULO\mprom <mp.romulo@gmail.com>
2022-08-12 16:14:10 -0300

    Model, DTO and mapping (DiscountAPI)

---
+++ VShop.DiscountApi/Mappings/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using VShop.DiscountApi.DTOs;
+using VShop.DiscountApi.Model;
+
+namespace VShop.DiscountApi.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<CouponDTO, Coupon>().ReverseMap();
+        }
+    }
+}
